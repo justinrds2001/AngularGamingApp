10f7acf8fa17249f356da1acc8ee335d6a18a1ec
unknown <ja.rodriguesdasilva@student.avans.nl>
2021-11-15 00:24:58 +0100

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { ColumnsComponent } from './pages/entity/columns.component';
+import { DetailComponent } from './pages/entity/detail/detail.component';
+import { EditComponent } from './pages/entity/edit/edit.component';
+import { GamesComponent } from './pages/entity/games/games.component';
+import { ListComponent } from './pages/entity/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'component-a' },
+  { path: 'component-a', component: AComponent },
+  { path: 'component-b', pathMatch: 'full', component: BComponent },
+  { path: 'users', pathMatch: 'full', component: ListComponent },
+  // users/new moet voor users/:id, omdat new anders als de id wordt gezien.
+  // Volgorde is belangrijk in routing.
+  { path: 'users/new', pathMatch: 'full', component: EditComponent },
+  { path: 'users/:id', pathMatch: 'full', component: DetailComponent },
+  { path: 'users/:id/edit', pathMatch: 'full', component: EditComponent },
+  {
+    path: 'columns',
+    component: ColumnsComponent,
+    children: [
+      { path: 'new', pathMatch: 'full', component: EditComponent },
+      { path: ':id', pathMatch: 'full', component: DetailComponent },
+      { path: ':id/edit', pathMatch: 'full', component: EditComponent },
+    ],
+  },
+  { path: 'games', pathMatch: 'full', component: GamesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListComponent } from './pages/entity/list/list.component';
+import { DetailComponent } from './pages/entity/detail/detail.component';
+import { ColumnsComponent } from './pages/entity/columns.component';
+import { EditComponent } from './pages/entity/edit/edit.component';
+import { GamesComponent } from './pages/entity/games/games.component';
+import { AddUserComponent } from './pages/entity/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponent,
+    BComponent,
+    NavComponent,
+    FooterComponent,
+    ListComponent,
+    DetailComponent,
+    ColumnsComponent,
+    EditComponent,
+    GamesComponent,
+    AddUserComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
